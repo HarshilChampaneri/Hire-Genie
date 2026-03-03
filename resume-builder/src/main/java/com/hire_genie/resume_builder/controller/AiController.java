c6df5cfa10d01689def38979828c27f6f8ee7c15
HarshilChampaneri <161130419+HarshilChampaneri@users.noreply.github.com>
2026-03-03 11:17:22 +0530

    Added Eureka Server, Config Server, and API Gateway

--- resume-builder/src/main/java/com/hire_genie/resume_builder/controller/AiController.java
+++ resume-builder/src/main/java/com/hire_genie/resume_builder/controller/AiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/resumes")
 public class AiController {
 
     private final AiService aiService;
